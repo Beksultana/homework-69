983a5b08dbea2a7d098707c7d6a501d4b96fed3f
sultan <aidaraliuulu@gmail.com>
2019-02-21 00:01:16 +0600

    В файле src создал папку containers & components & store. В файле store создал папку actions & reducers. В файле reducers создал файл products-reducer.js.

---
+++ src/store/reducers/products-reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    ingredients: {
+        name: '',
+        cost: '',
+        image: '',
+    }
+};
+
+const reducer = (state = initialState, action) => {
+
+    return state;
+};
+
+export default reducer;
